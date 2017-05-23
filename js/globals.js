40b45403ac5136a7a1088a5c4d3f355a452371da
Jelle S <jellejstuurman@gmail.com>
2017-05-23 15:17:29 +0200

    disable console.log on command

--- js/globals.js
+++ js/globals.js
@@ -1,15 +1,14 @@
-var DEBUG_MODE = false; // Set this value to false for production
+//console log
+	var DEBUG_MODE = false; // Set this value to false for production
+	if(typeof(console) === 'undefined') {
+	   console = {}
+	}
+	if(!DEBUG_MODE || typeof(console.log) === 'undefined') {
+	   // FYI: Firebug might get cranky...
+	   console.log = console.error = console.info = console.debug = console.warn = console.trace = console.dir = console.dirxml = console.group = console.groupEnd = console.time =    console.timeEnd = console.assert = console.profile = function() {};
+	}
+	var log = console.log.bind(console)
 
-if(typeof(console) === 'undefined') {
-   console = {}
-}
-
-if(!DEBUG_MODE || typeof(console.log) === 'undefined') {
-   // FYI: Firebug might get cranky...
-   console.log = console.error = console.info = console.debug = console.warn = console.trace = console.dir = console.dirxml = console.group = console.groupEnd = console.time =    console.timeEnd = console.assert = console.profile = function() {};
-}
-
-var log = console.log.bind(console)
 //var localTesting
 
 //var ifMobile = true
